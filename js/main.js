032f00e066ed2d72c16c9b2a6817fc921c585fc1
lightbluecactus <violetday77@yahoo.com>
2021-01-13 14:53:23 -0400

    adding JS and CSS

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//this is a JavaScript module syntax
+// IIFE (this is the technical name)
+(() => {
+  // stub
+  console.log('JS is linked! Hooray!');
+
+  let theButton = document.querySelector(".icon-button");
+    theIcons = document.querySelectorAll(".icon");
+
+  function logClick() {
+    console.log('clicked the button (or icon)!');
+  }
+
+  theButton.addEventListener('click', logClick);
+  theIcons.forEach(icon => icon.addEventListener('click', logClick));
+
+})();
